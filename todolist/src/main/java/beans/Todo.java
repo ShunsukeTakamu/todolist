6a3a4b94194332c6b71f5b76ba0d1d0a7755099a
User <User@WINDOWS-2IB8PTP>
2025-06-03 17:11:32 +0900

    lombok

--- todolist/src/main/java/beans/Todo.java
+++ todolist/src/main/java/beans/Todo.java
@@ -8,9 +8,12 @@ import lombok.NoArgsConstructor;
 
 @Data @NoArgsConstructor @AllArgsConstructor
 public class Todo {
-    private int id;
-    private String title;
-    private LocalDate dueDate;
-    private String assignee;
-    private boolean done;
+	private int id;
+	private String title;
+	private LocalDate dueDate;
+	private String assignee;
+	private boolean done;
+	private LocalDate created_at;
+	private LocalDate completed_at_at;
+
 }
